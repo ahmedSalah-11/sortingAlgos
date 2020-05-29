ba997166a05e527513eb06c1dcb3f4c230211c1b
ahmedSalah-11 <ahmedsalah@stud.cu.edu.eg>
2020-05-29 15:56:44 +0200

    Updates

---
+++ precache-manifest.d62a20d9b717e8bcf72f5e91c9c77e7f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da62f7018400c71bb6ca715ff6cc1813",
+    "url": "/sortingAlgos/index.html"
+  },
+  {
+    "revision": "c63d2bca875d298efd44",
+    "url": "/sortingAlgos/static/css/2.53823a3d.chunk.css"
+  },
+  {
+    "revision": "482d90868044c9153a55",
+    "url": "/sortingAlgos/static/css/main.10b35f6c.chunk.css"
+  },
+  {
+    "revision": "c63d2bca875d298efd44",
+    "url": "/sortingAlgos/static/js/2.3a0970e5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/sortingAlgos/static/js/2.3a0970e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "482d90868044c9153a55",
+    "url": "/sortingAlgos/static/js/main.2d12b6c8.chunk.js"
+  },
+  {
+    "revision": "a3f0dda34be50018a518",
+    "url": "/sortingAlgos/static/js/runtime-main.2863f5b1.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sortingAlgos/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/sortingAlgos/precache-manifest.d62a20d9b717e8bcf72f5e91c9c77e7f.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/sortingAlgos/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
